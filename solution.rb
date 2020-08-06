11fdb5487ba8add1fb11cd89d214014b5d0cbd7a
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-08-06 14:02:45 +0000

    Automatically backed up by Learn

--- solution.rb
+++ solution.rb
@@ -1,6 +1,21 @@
+
 class Deck
 
-end
+  def initialize
+
+    @deck = []
+    suits = ["spades", "diamonds", "hearts", "clubs"]
+    ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    
+    suits.each do |suit|
+      ranks.each do |rank|
+        card = Card.new(rank, suit)
+        @deck << card
+      end
+    end
+  end
+
+
 
 class Card
 
