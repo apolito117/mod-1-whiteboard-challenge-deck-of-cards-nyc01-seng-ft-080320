e4ad66a6bebb4394d9cdd706af53a97513fa3b30
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-08-06 14:02:49 +0000

    Automatically backed up by Learn

--- solution.rb
+++ solution.rb
@@ -6,7 +6,7 @@ class Deck
     @deck = []
     suits = ["spades", "diamonds", "hearts", "clubs"]
     ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-    
+
     suits.each do |suit|
       ranks.each do |rank|
         card = Card.new(rank, suit)
